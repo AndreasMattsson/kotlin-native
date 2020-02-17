c7ebfc2df7b205b2190d7b65d42931a9b2ac9942
Andreas Mattsson <per.andreas.mattsson@gmail.com>
2020-02-17 09:01:34 +0100

    trimmed some fat

--- backend.native/tests/interop/objc_with_initializer/objc_test.kt
+++
@@ -1,7 +0,0 @@
-import objc_misc.*
-
-val a = B.giveC()!! as C
-
-fun main() {
-  println("OK")
-}
--- platformLibs/src/platform/osx/set_depends.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../../../../dist/bin/run_konan defFileDependencies -target macos_x64 *.def
